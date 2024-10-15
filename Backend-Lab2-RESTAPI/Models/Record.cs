444540eebe91dbffd3c59586185e193132efdd6c
D0wnfal1 <deadofgreaf@gmail.com>
2024-10-15 23:54:10 +0300

    Add Domain Models

---
+++ Backend-Lab2-RESTAPI/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_Lab1_RESTAPI.Models
+{
+	public class Record
+	{
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Backend-Lab2-RESTAPI/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Lab1_RESTAPI.Models
+{
+	public class User
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
